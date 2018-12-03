2e6d1753138e5870d323717150dfbab4f7c7dbe7
Dominik Soczewka <vileen@github.com>
2018-12-03 21:47:59 +0100

    added propTypes

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Return node(s) with the given data-test value
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
@@ -6,4 +8,9 @@
  */
 export const findByTestAttr = (wrapper, val) => {
     return wrapper.find(`[data-test='${val}']`)
+};
+
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
+    expect(propError).toBeUndefined();
 };
