1e49b27cf3230ae90a37b431fcceadba683fdc62
Dominik Soczewka <vileen@github.com>
2018-12-04 15:15:36 +0100

    added reduxThunk to tests and app

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,12 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 export const storeFactory = initialState => {
-    return createStore(rootReducer, initialState);
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
